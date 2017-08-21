c223bbd9f30f9d4b45f1dc594cd306630ad6f35e
Kenneth R. Hill <khill@us.ibm.com>
2017-08-21 17:40:09 -0500

    Added APLv2
    Signed-off-by: Kenneth R. Hill <khill@us.ibm.com>

--- docker_ubuntu/install-ubuntu-tiger.sh
+++ docker_ubuntu/install-ubuntu-tiger.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -ex
+#
+# Copyright 2017 International Business Machines
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 # VGL64="http://sourceforge.net/projects/virtualgl/files/2.5/virtualgl_2.5_amd64.deb/download"
 dirname=$(dirname $0)
